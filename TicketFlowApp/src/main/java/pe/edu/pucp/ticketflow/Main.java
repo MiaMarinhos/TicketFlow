f9382fe651d935fded3ba0fa9314aa8b8198e009
Emiliano <a20206459@pucp.edu.pe>
2026-04-21 15:36:19 -0500

    Conexion BD

--- TicketFlowApp/src/main/java/pe/edu/pucp/ticketflow/Main.java
+++ TicketFlowApp/src/main/java/pe/edu/pucp/ticketflow/Main.java
@@ -1,7 +1,26 @@
 package pe.edu.pucp.ticketflow;
 
+import pe.edu.pucp.ticketflow.dao.manager.DBManager;
+
+import java.sql.Connection;
+
 public class Main {
-    static void main() {
-        //TODO Implementar 4 entidades
+    public static void main(String[] args) {
+        //PRUEBA DE CONEXION BD
+        /*try {
+            Connection conn = DBManager.getInstance().getConnection();
+
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("✅ Conexión exitosa a la base de datos");
+            } else {
+                System.out.println("❌ No se pudo establecer la conexión");
+            }
+
+        } catch (Exception e) {
+            System.out.println("❌ Error en la conexión:");
+            e.printStackTrace();
+        }*/
+
+        //SEGUIR AQUI
     }
 }
