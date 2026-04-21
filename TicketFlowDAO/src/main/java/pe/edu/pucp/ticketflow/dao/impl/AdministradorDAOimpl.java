a6f76c8922e3e28bbb41f992e00202e4f4d5a885
MiaMarinhos <a20230411@pucp.edu.pe>
2026-04-21 09:08:44 -0500

    +UsuarioDAOImpl

--- TicketFlowDAO/src/main/java/pe/edu/pucp/ticketflow/dao/impl/AdministradorDAOimpl.java
+++ TicketFlowDAO/src/main/java/pe/edu/pucp/ticketflow/dao/impl/AdministradorDAOimpl.java
@@ -11,18 +11,18 @@ import java.util.List;
 public class AdministradorDAOimpl implements AdministradorDAO {
     @Override
     public void create(Administrador administrador){
-
+        String sql = "";
     }
     @Override
     public Administrador read(Integer id){
-
+        String sql = "";
     }
     @Override
     public void update(Administrador administrador){
-
+        String sql = "";
     }
     @Override
     public void delete(Integer id){
-
+        String sql = "";
     }
 }
--- TicketFlowDAO/src/main/java/pe/edu/pucp/ticketflow/dao/impl/ClienteDAOimpl.java
+++ TicketFlowDAO/src/main/java/pe/edu/pucp/ticketflow/dao/impl/ClienteDAOimpl.java
@@ -1,4 +1,23 @@
 package pe.edu.pucp.ticketflow.dao.impl;
 
-public class ClienteDAOimpl {
+import pe.edu.pucp.ticketflow.dao.ClienteDAO;
+import pe.edu.pucp.ticketflow.usuario.model.Cliente;
+
+public class ClienteDAOimpl implements ClienteDAO {
+    @Override
+    public void create(Cliente administrador){
+        String sql = "";
+    }
+    @Override
+    public Cliente read(Integer id){
+        String sql = "";
+    }
+    @Override
+    public void update(Cliente administrador){
+        String sql = "";
+    }
+    @Override
+    public void delete(Integer id){
+        String sql = "";
+    }
 }
