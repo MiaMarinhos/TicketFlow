2e3a6bb29638f85701633411e9d89e65c6bdf21a
MiaMarinhos <a20230411@pucp.edu.pe>
2026-04-21 18:56:44 -0500

    CompraDAOimpl

--- TicketFlowDAO/src/main/java/pe/edu/pucp/ticketflow/dao/impl/CompraDAOimpl.java
+++ TicketFlowDAO/src/main/java/pe/edu/pucp/ticketflow/dao/impl/CompraDAOimpl.java
@@ -1,11 +1,18 @@
 package pe.edu.pucp.ticketflow.dao.impl;
 
 import pe.edu.pucp.ticketflow.compra.model.Compra;
+import pe.edu.pucp.ticketflow.compra.model.EstadoCompra;
+import pe.edu.pucp.ticketflow.compra.model.MetodoPago;
 import pe.edu.pucp.ticketflow.dao.CompraDAO;
 import pe.edu.pucp.ticketflow.dao.manager.DBManager;
+import pe.edu.pucp.ticketflow.evento.model.Evento;
+import pe.edu.pucp.ticketflow.puntos_bonus.model.PuntosBonus;
+import pe.edu.pucp.ticketflow.usuario.model.Cliente;
 
-import java.util.Date;
 import java.sql.*;
+import java.time.LocalDate;
+import java.util.LocalDate;
+
 
 public class CompraDAOimpl implements CompraDAO {
     @Override
@@ -50,7 +57,26 @@ public class CompraDAOimpl implements CompraDAO {
             ps.setInt(1, id);
             try (ResultSet rs = ps.executeQuery()){
                 if(rs.next()){
-                    //TODO
+                    compra.setIdCompra(rs.getInt("idCompras"));
+                    compra.setCantidadEntradas(rs.getInt("entradas_compradas"));
+                    compra.setFechaCompra(rs.getDate("fecha_compra").toLocalDate());
+                    compra.setMetodo(MetodoPago.valueOf(rs.getString("metodo_pago")));
+                    compra.setHoraCompra(rs.getTime("hora_compra").toLocalTime());
+                    compra.setEstado(EstadoCompra.valueOf(rs.getString("estado")));
+                    compra.setMontoParcial(rs.getDouble("monto_parcial"));
+                    compra.setMontoTotal(rs.getDouble("monto_total"));
+
+                    PuntosBonus puntosBonus = new PuntosBonus();
+                    puntosBonus.setIdPuntosBonus(rs.getInt("idPuntos_bonus"));
+                    compra.setPuntosBonus(puntosBonus);
+
+                    Cliente cliente = new Cliente();
+                    cliente.setIdUsuario(rs.getInt("idCliente"));
+                    compra.setCli(cliente);
+
+                    Evento evento = new Evento();
+                    evento.setIdEvento(rs.getInt("idEvento"));
+                    compra.setEve(evento);
 
                     return compra;
                 }
@@ -62,10 +88,22 @@ public class CompraDAOimpl implements CompraDAO {
     }
     @Override
     public Compra update(Compra compra){
-        String sql = "UPDATE compras SET idCompras=?, entradas_compradas=?, fecha_compra=?, metodo_pago=?, hora_compra=?, estado=?, monto_parcial=?, monto_total=?, idPuntos_bonus=?, idCliente=?, idEvento=? WHERE idCompras=?";
+        String sql = "UPDATE compras SET entradas_compradas=?, fecha_compra=?, metodo_pago=?, hora_compra=?, estado=?, monto_parcial=?, monto_total=?, idPuntos_bonus=?, idCliente=?, idEvento=? WHERE idCompras=?";
         try (Connection con = DBManager.getInstance().getConnection();
              PreparedStatement ps = con.prepareStatement(sql)){
-            //TODO
+
+            ps.setInt(1, compra.getCantidadEntradas());
+            ps.setDate(2, java.sql.Date.valueOf(compra.getFechaCompra()));
+            ps.setString(3, compra.getMetodo().name());
+            ps.setTime(4, java.sql.Time.valueOf(compra.getHoraCompra()));
+            ps.setString(5, compra.getEstado().name());
+            ps.setDouble(6, compra.getMontoParcial());
+            ps.setDouble(7, compra.getMontoTotal());
+            ps.setInt(8, compra.getPun().getIdPuntosBonus());
+            ps.setInt(9, compra.getCli().getIdUsuario());
+            ps.setInt(10, compra.getEve().getIdEvento());
+
+            ps.setInt(11, compra.getIdCompra());
 
             int affectedRows = ps.executeUpdate();
             if(affectedRows==0){
