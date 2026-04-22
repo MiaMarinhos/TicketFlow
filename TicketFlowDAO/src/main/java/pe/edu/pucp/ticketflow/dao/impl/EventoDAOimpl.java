4903a324e14a925b5f8bb26502331619709da21d
fabian40l <Fabian_Orihuela_2301@hotmail.com>
2026-04-21 20:41:12 -0500

    EventoDAOimpl terminado

--- TicketFlowDAO/src/main/java/pe/edu/pucp/ticketflow/dao/impl/EventoDAOimpl.java
+++ TicketFlowDAO/src/main/java/pe/edu/pucp/ticketflow/dao/impl/EventoDAOimpl.java
@@ -33,7 +33,7 @@ public class EventoDAOimpl implements EventoDAO {
 
         try(Connection con = DBManager.getInstance().getConnection();
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
-            //TODO
+            //
             ps.setInt(1, evento.getIdEvento());
             ps.setString(2,evento.getNombreEvento());
             ps.setString(3,evento.getDescripcion());
@@ -122,12 +122,56 @@ public class EventoDAOimpl implements EventoDAO {
         return null;    }
 
     @Override
-    public Evento update(Evento t) {
-        return null;
+    public Evento update(Evento evento) {
+        String sql = "UPDATE Distrito SET nombre=?, idRegion=?  WHERE idDistrito=?";
+        try (Connection con = DBManager.getInstance().getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)){
+
+            ps.setInt(1, evento.getIdEvento());
+            ps.setString(2,evento.getNombreEvento());
+            ps.setString(3,evento.getDescripcion());
+            ps.setInt(4,evento.getCapacidadEntradas());
+            ps.setString(5,evento.getCat().name());
+            ps.setDate(6, valueOf(evento.getFecha()));
+            ps.setTime(7, java.sql.Time.valueOf(evento.getHoraInicio()));
+            ps.setTime(8,java.sql.Time.valueOf(evento.getHoraFin()));
+            ps.setString(9, evento.getUbicacion());
+            ps.setString(10,evento.getNombreEstablecimiento());
+            ps.setString(11, evento.getEstadoPublicacion().name());
+            ps.setString(12,evento.getEstado().name());
+            ps.setString(13, evento.getUrlPoster());
+            ps.setDouble(14,evento.getPrecioEntrada());
+            ps.setInt(15,evento.getEntradasAdquiridas());
+            ps.setInt(16,evento.getCapacidadEntradas()-evento.getEntradasAdquiridas());
+            ps.setDouble(17, evento.getTotalRecaudado());
+            ps.setDouble(18,evento.getTotalReal());
+            ps.setInt(19,evento.getDiscrito().getRegion().getIdRegion());
+            ps.setInt(20,evento.getDiscrito().getIdDistrito());
+            ps.setInt(21,evento.getAnfi().getIdUsuario());
+
+            int affectedRows = ps.executeUpdate();
+            if(affectedRows==0){
+                throw new RuntimeException("No se encontro el Distrito");
+            }
+            return evento;
+        } catch (SQLException e){
+            throw new RuntimeException("No se pudo actualizar el Distrito", e);
+        }
     }
 
     @Override
-    public void delete(Integer integer) {
-
+    public void delete(Integer id) {
+        String sql = "DELETE FROM compras WHERE idCompras=?";
+        try (Connection con = DBManager.getInstance().getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setInt(1, id);
+            int affectedRows = ps.executeUpdate();
+            if(affectedRows==0){
+                throw new RuntimeException("No se encontro la Compra");
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("No se pudo eliminar la Compra", e);
+        }
     }
+
 }
